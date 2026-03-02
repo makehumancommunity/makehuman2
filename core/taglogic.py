c0aaf07f057f66fc3e204f51d40095238880f3eb
black-punkduck <ralf-rednitzhembach@t-online.de>
2026-03-02 20:20:15 +0100

    improved tag logic by name,
    longest fitting tag will be checked first, so bathingsuit before suit,
    this will allow more "GuessName" entries in the filter file

--- core/taglogic.py
+++ core/taglogic.py
@@ -11,73 +11,97 @@ class tagLogic():
         self.reserved = ["Translate", "GuessName", "Shortcut"]
         self.tagreplace = {}
         self.tagfromname = {}
+        self.fromnamesorted = []        # sorted keys for tagfromname by length
         self.tagproposals = []
         self.json = json
 
     def proposals(self):
-        return(self.tagproposals)
+        return self.tagproposals
 
     def convertJSON(self, json, separator=":", prepend = ""):
-        for elem in json:
-            if elem in self.reserved:
+        """
+        convert JSON to a list of proposals like 'slot:top-torso:layer3:jacket'
+
+        :param dict json: json dictionary
+        :param str separator: separactor for layers, usually ':'
+        :param str prepend: string to be prepended
+        """
+        for key, item in json.items():
+            if key in self.reserved:
                 continue
-            j = json[elem]
-            if type(j) is dict:
-                self.convertJSON(j, separator, prepend + elem + separator)
-            elif type(j) is list:
-                for l in j:
-                    self.tagproposals.append((prepend + elem + separator + l).lower())
+            if type(item) is dict:
+                self.convertJSON(item, separator, prepend + key + separator)
+            elif type(item) is list:
+                for l in item:
+                    self.tagproposals.append((prepend + key + separator + l).lower())
 
     def createTagGroups(self, subtree, path):
         """
-        create texts to prepend certain tags, can also translate tags
+        create texts from selection filter (JSON), prepends path if available
+
+        :param dict subtree: part or complete JSON dictionary of selection_filter
+        :param str path: a path separated by ':' like :slot:bottom
         """
-        for elem in subtree:
-            if isinstance(elem, str):
-                if elem == "Translate":                             # extra, change by word
-                    for l in subtree[elem]:
-                        self.tagreplace[l.lower()] = subtree[elem][l]
+        for key, item in subtree.items():
+
+            if isinstance(key, str):
+                if key == "Translate":                             # extra, insert into tagreplace dictionary
+                    for l in item:
+                        self.tagreplace[l.lower()] = item[l]
                     continue
-                if elem == "GuessName":                             # extra, change by word
-                    for l in subtree[elem]:
-                        self.tagfromname[l.lower()] = subtree[elem][l]
+
+                if key == "GuessName":                             # extra, insert into tagfromname dictionary
+                    for l in item:
+                        self.tagfromname[l.lower()] = item[l]
                     continue
-                if isinstance(subtree[elem], dict):
-                    self.createTagGroups(subtree[elem], path + ":" + elem.lower())
-                elif isinstance(subtree[elem], list):
-                    if elem == "Shortcut":
+
+                if isinstance(item, dict):
+                    self.createTagGroups(item, path + ":" + key.lower())    # next subtree
+
+                elif isinstance(item, list):
+                    if key == "Shortcut":
                         pass
                     else:
-                        for l in subtree[elem]:
-                            repl = path + ":" + elem.lower()
+                        for l in item:                              # insert into tagreplace dictionary
+                            repl = path + ":" + key.lower()
                             self.tagreplace[l.lower()] = repl[1:]       # get rid of first ":"
+
+        if path == "":
+            self.fromnamesorted = sorted(self.tagfromname, key=len, reverse=True)
 
     def completeTags(self, name, tags):
         """
-        replace tags by tags with prepended strings or check name
+        replace original tags if needed, append those not found or check name and create tag from that
+        all done in lower case
+
+        :param str name: name of asset
+        :param list tags: tags to be tested
+        :return: new tags
         """
         newtags = []
         for tag in tags:
             ltag = tag.lower()
-            if ltag in self.tagreplace:
+            if ltag in self.tagreplace:             # in case it needs to be replaced
                 elem = self.tagreplace[ltag]
                 if elem is not None:
-                    if elem.startswith("="):        # complete replacement
+                    if elem.startswith("="):        # if replacement starts with '=' replace complete tag
                         ntag = elem[1:]
-                    else:
+                    else:                           # else append tag
                         ntag = elem+":"+ltag
-                    if ntag not in newtags:
+                    if ntag not in newtags:         # only append if new
                         newtags.append(ntag)
             else:
-                if tag not in newtags:
+                if tag not in newtags:              # append tags not found and not yet available
                     newtags.append(tag)
 
-        for tag in self.tagfromname:
+        for tag in self.fromnamesorted:             # check for longest fitting name first (bathingsuit before suit)
             if tag in name:
                 ntag = self.tagfromname[tag]
-                if ntag not in newtags:
+                if ntag not in newtags:             # append that tag and stop
                     newtags.append(ntag)
-        return (newtags)
+                    break
+
+        return newtags
 
     def create(self):
         self.convertJSON(self.json)
